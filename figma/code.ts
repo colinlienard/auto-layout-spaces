4eef940cea72e602258b9499bb0665bda5b28e4c
Colin Lienard <colin.lienard87@orange.fr>
2022-08-17 21:19:57 +0200

    feat(core): work on creating spaces

--- figma/code.ts
+++ figma/code.ts
@@ -1,7 +1,23 @@
 figma.showUI(__html__);
 
-figma.ui.onmessage = (message: string) => {
-  console.log(message);
+figma.ui.onmessage = () => {
+  const nodes = figma.currentPage.findAll(
+    (node) => node.type === 'FRAME' && node.layoutMode !== 'NONE'
+  ) as FrameNode[];
+
+  nodes.forEach((node) => {
+    const frame = figma.createFrame();
+    frame.x = node.x;
+    frame.y = node.y;
+    frame.resize(node.width, node.paddingTop);
+    frame.locked = true;
+
+    const { width, height } = node;
+
+    node.appendChild(frame);
+    frame.layoutPositioning = 'ABSOLUTE';
+    node.resize(width, height);
+  });
 
   figma.closePlugin();
 };
