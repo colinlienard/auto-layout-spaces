e7b1630cb8bcd950e54473bde3d4bda0b57db868
Colin Lienard <colin.lienard87@orange.fr>
2022-08-17 18:41:54 +0200

    chore: add and setup ESLint

--- figma/code.ts
+++ figma/code.ts
@@ -2,6 +2,6 @@ figma.showUI(__html__);
 
 figma.ui.onmessage = (message: string) => {
   console.log(message);
-  
+
   figma.closePlugin();
 };
--- ui/main.ts
+++ ui/main.ts
@@ -1,5 +1,3 @@
-console.log('test');
-
 document.querySelector('button')?.addEventListener('click', () => {
   parent.postMessage({ pluginMessage: 'salut' }, '*');
 });
