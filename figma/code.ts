3f8c2e95aa6f551b774e2d17628d8ad9a91966a1
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2022-08-18 18:20:53 +0200

    feat(core): view spaces in auto-layouts (#4)
    * chore: fix `build` and `watch` scripts
    
    * chore: fix linting issues
    
    * feat(core): work on creating spaces
    
    * feat(core): show paddings
    
    * feat(core): show spaces between items
    
    * feat(core): append all visual spaces in a group

--- figma/code.ts
+++ figma/code.ts
@@ -1,7 +1,144 @@
+import { rgba } from './helpers';
+
 figma.showUI(__html__);
 
-figma.ui.onmessage = (message: string) => {
-  console.log(message);
+// Create a frame with a text in it
+const createVisualSpace = (
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  unit: number,
+  color: [number, number, number]
+): FrameNode => {
+  const frame = figma.createFrame();
 
-  figma.closePlugin();
+  // Set parameters
+  frame.x = x;
+  frame.y = y;
+  frame.layoutMode = 'HORIZONTAL';
+  frame.primaryAxisAlignItems = 'CENTER';
+  frame.counterAxisAlignItems = 'CENTER';
+  frame.clipsContent = false;
+  frame.fills = rgba(...color, 0.2);
+  frame.expanded = false;
+
+  // Create a text centered in the frame
+  const text = figma.createText();
+  text.characters = `${unit}px`;
+  text.fills = rgba(...color);
+  text.strokes = rgba(1, 1, 1);
+  frame.appendChild(text);
+
+  // Set the size after appending the text
+  frame.resize(width, height);
+
+  return frame;
+};
+
+figma.ui.onmessage = async () => {
+  // Load a font to display text
+  await figma.loadFontAsync({ family: 'Inter', style: 'Regular' });
+
+  // Get all frames with auto-layouts
+  const nodes = figma.currentPage.findAll(
+    (node) => node.type === 'FRAME' && node.layoutMode !== 'NONE'
+  ) as FrameNode[];
+
+  // For each node, create visual space for each padding
+  nodes.forEach((node) => {
+    const {
+      x,
+      y,
+      width,
+      height,
+      children,
+      itemSpacing,
+      paddingBottom,
+      paddingLeft,
+      paddingRight,
+      paddingTop,
+    } = node;
+
+    const allVisualSpaces: FrameNode[] = [];
+
+    // Create visual spaces for each space between children
+    if (children.length > 1) {
+      children.forEach((child, index) => {
+        // Do not add a visual space fot the last child
+        if (index === children.length - 1) {
+          return;
+        }
+
+        // Vertical spaces
+        if (node.layoutMode === 'VERTICAL') {
+          allVisualSpaces.push(
+            createVisualSpace(
+              x + paddingLeft,
+              y + child.y + child.height,
+              width - paddingLeft - paddingRight,
+              itemSpacing,
+              itemSpacing,
+              [0, 0, 1]
+            )
+          );
+          return;
+        }
+
+        // Horizontal spaces
+        allVisualSpaces.push(
+          createVisualSpace(
+            x + child.x + child.width,
+            y + paddingTop,
+            itemSpacing,
+            height - paddingTop - paddingBottom,
+            itemSpacing,
+            [0, 0, 1]
+          )
+        );
+      });
+    }
+
+    // Create visual spaces for paddings
+    if (paddingTop) {
+      allVisualSpaces.push(
+        createVisualSpace(x, y, width, paddingTop, paddingTop, [1, 0, 0])
+      );
+    }
+    if (paddingRight) {
+      allVisualSpaces.push(
+        createVisualSpace(
+          x + width - paddingRight,
+          y,
+          paddingRight,
+          height,
+          paddingRight,
+          [1, 0, 0]
+        )
+      );
+    }
+    if (paddingBottom) {
+      allVisualSpaces.push(
+        createVisualSpace(
+          x,
+          y + height - paddingBottom,
+          width,
+          paddingBottom,
+          paddingBottom,
+          [1, 0, 0]
+        )
+      );
+    }
+    if (paddingLeft) {
+      allVisualSpaces.push(
+        createVisualSpace(x, y, paddingLeft, height, paddingLeft, [1, 0, 0])
+      );
+    }
+
+    // Put all visual spaces in a group
+    const group = figma.group(allVisualSpaces, figma.currentPage);
+    group.expanded = false;
+    group.locked = true;
+    group.name = '< 👀 Auto-layout Spaces >';
+  });
 };
